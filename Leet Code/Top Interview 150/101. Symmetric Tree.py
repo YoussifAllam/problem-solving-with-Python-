2bfb48783e3c3cad5c7ca9b15f8cd254afcd9dba
YoussifAllam <96921160+YoussifAllam@users.noreply.github.com>
2023-08-07 12:00:08 +0300

    Add files via upload

---
+++ Leet Code/Top Interview 150/101. Symmetric Tree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        if not root:
+            return True
+        return self.miror(root.left, root.right)
+    
+    def miror(self,left,right):
+        if not left and not right:
+            return True
+        if not left or not right:
+            return False
+        return left.val == right.val and self.isMirror(left.left, right.right) and self.isMirror(left.right, right.left)
+            
